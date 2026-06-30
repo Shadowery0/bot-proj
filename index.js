dfe5da33ff86f8f32cd757211cb13e567e4dbd3a
Shadowery0 <trienglutamate@gmail.com>
2026-06-29 21:07:19 -0400

    commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+const {PassThrough, Writable} = require("stream")
+const {Chalk} = require("chalk")
+require("dotenv").config()
+const chalk = new Chalk({level: 2})
+
+class SimpleLogger {
+  stream
+  fstream
+  constructor(_csl, _fle) {
+    if(!(_fle instanceof Writable)) {
+      _fle = new PassThrough()
+    }
+    this.stream = _csl
+    this.fstream = _fle
+  }
+  error(data) {
+    this.stream.error(chalk.bgRed.white(`[ERR] [${new Date().toISOString()}]`) + " " + data.toString())
+    fstream.write(`[ERR] [${new Date().toISOString()}] ` + data.toString())
+  }
+  warn(data) {
+    this.stream.warn(chalk.bgYellowBright.white(`[WARN] [${new Date().toISOString()}]`)+ " " + data.toString())
+    fstream.write(`[WARN] [${new Date().toISOString()}] ` + data.toString())
+  }
+  info(data) {
+    this.stream.log(chalk.bgGray.black(`[INFO] [${new Date().toISOString()}]`) + " " + data.toString())
+    fstream.write(`[INFO] [${new Date().toISOString()}] ` + data.toString())
+  }
+  debug(data) {
+    this.stream.debug(chalk.bgCyan.white(`[DBG] [${new Date().toISOString()}]`) + " " + data.toString())
+    fstream.write(`[DBG] [${new Date().toISOString()}] ` + data.toString())
+  }
+}
+
+class BotWrapper {
+  client = new Client({intents: 3191159});
+  logger = new SimpleLogger(console);
+  constructor(token) {
+    this.client.login(token)
+      .then(_ => {console.debug("Logged in! Now I am " + this.client.user.tag + " or "+ this.client.user.id)})
+      .catch(console.error)
+    this.client.on("debug", _ => {console.debug(_)})
+  }
+}
+
+const bot = new BotWrapper(process.env.TOKEN)
