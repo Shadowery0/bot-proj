7fe2f7ccd4e387d734f824c4da31c50825d64af4
Shadowery0 <trienglutamate@gmail.com>
2026-06-30 10:36:32 -0400

    update cmd.js

--- cmd.js
+++ cmd.js
@@ -4,8 +4,17 @@ const { REST, Routes } = require("discord.js")
 require("dotenv").config()
 
 let cmd = []
-let dir = fs.readdirSync(path.join(__dirname, "commands/"))
+let dir = fs.readdirSync(path.join(__dirname, "commands/"), {withFileTypes: true})
 
 for(const item of dir) {
-  console.log(item)
+  if(item.isDirectory()) {
+    
+  } else {
+    const file = require(path.join(__dirname, "commands", item.name))
+    cmd.push(file.data?.toJSON())
+  }
 }
+
+let rest = new REST().setToken(process.env.TOKEN)
+
+rest.put(Routes.applicationGuildCommand(process.env.BOT_ID, process.env.CAEID), {body: cmd})
