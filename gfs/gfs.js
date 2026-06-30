cb3e8ec44c68763ca1fa89598b473f96cf41b66b
Shadowery0 <trienglutamate@gmail.com>
2026-06-30 01:36:28 -0400

    update gfs.js

--- gfs/gfs.js
+++ gfs/gfs.js
@@ -36,7 +36,7 @@ class ClientPlayer extends Player {
           if(!!_r?.data && _r?.data !== undefined && _r?.data !== "" && _r?.data !== null) {
             re(_r)
             console.log("Connected to GeoFS. I am " + _r.data.myId)
-            this._self["id"] = _r.data.myId
+            this._self["id"] = _r.data.myId.toString()
           } else {
             console.log("Null resp.")
             rj(null)
