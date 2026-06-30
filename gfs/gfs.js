83df6a5a58f1d9509f64c888dd74352efe030a9a
Shadowery0 <trienglutamate@gmail.com>
2026-06-29 23:12:12 -0400

    git cmd

--- gfs/gfs.js
+++ gfs/gfs.js
@@ -17,45 +17,41 @@ class Player {
 
 class ClientPlayer extends Player {
   users = []
-  async #tryCon() {
+  async _tryCon() {
     try {
-      const r = (await axios.post("https://mps.geo-fs.com/update", this._self.toJSON())).toJSON()
-      console.log(r)
+      const r = await axios.post("https://mps.geo-fs.com/update", this._self)
+      console.log(r.data)
       return r
     } catch(e) {
       throw new Error("Request Rejected: " + e)
     }
   }
-  
+  init() {
+    return new Promise((re, rj) => {
+      let r = null;
+      const _r = this._tryCon()
+        .then(__r => { })
+        .catch(e => console.error(e))
+    })
+  }
   constructor(acid, sid) {
     super({
       "origin": "https://www.geo-fs.com",
       "acid": acid,
       "sid": sid,
       "id": "",
-      "ac": "1",
-      "co": [9999999999999999]*6,
-      "ve": [0.0]*6,
-      "st": {"gr": true, "as": 0},
-      "ti": int(time.time() * 1000),
       "ac": 1,
       "co": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
       "ve": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
       "st": {"gr": 1, "as": 0},
       "ro": {"ad": 0},
-      "ti": int(Date.now() * 1000),
+      "ti": Date.now() * 1000,
       "m": "",
       "ci": 0
     });
     
-    return new Promise((re, rj) => {
-      const r = this.#tryCon()
-      if(!!r) {
-        re(r)
-      } else {
-        rj("Null response")
-      }
-    })
+    this.init()
+      .catch(e => console.error(e))
   }
 }
 
