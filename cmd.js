a8658c30c5ef4d0a25aaf7dbde321de0fda130fb
Shadowery0 <trienglutamate@gmail.com>
2026-06-30 09:47:41 -0400

    update cmd.js

--- cmd.js
+++ cmd.js
@@ -4,7 +4,7 @@ const { REST, Routes } = require("discord.js")
 require("dotenv").config()
 
 let cmd = []
-let dir = fs.readdirSync(path.join(__dirname, "cmds/"))
+let dir = fs.readdirSync(path.join(__dirname, "commands/"))
 
 for(item in dir) {
   console.log(item)
