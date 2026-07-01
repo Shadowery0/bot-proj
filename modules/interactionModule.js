42b7474edafb5e5b56534f08e051f01ad682fa18
Shadowery0 <trienglutamate@gmail.com>
2026-07-01 07:05:22 -0400

    update interactionModule.js

--- modules/interactionModule.js
+++ modules/interactionModule.js
@@ -3,5 +3,7 @@ module.exports = {
   event: "interactionCreate",
   handler: (i) => {
     console.log("Received an interaction from user " + i.member.id)
+    i.deferReply()
+      .then(_ => _.interaction.reply("Test"))
   }
 }
