f193f66bc57435a75e445e7c45b0d1a480e3d338
Shadowery0 <trienglutamate@gmail.com>
2026-07-01 09:21:04 -0400

    update api.js

--- commands/api.js
+++ commands/api.js
@@ -5,6 +5,9 @@ const bot = require(path.resolve(__dirname, "../", "index.js"))
 module.exports = {
   handler: async (i) => {
     await i.deferReply()
+    if(bot.geofs?.map === undefined || bot.geofs?.map === null) {
+      i.editReply("Please try again.")
+    }
     await i.editReply(`There are ${bot.geofs.map.getPlayerCount()} players online`)
   },
   data: new SlashCommandBuilder()
