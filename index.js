3e4079641238f8802f95978fc8091d12117f3f86
Shadowery0 <trienglutamate@gmail.com>
2026-07-01 03:44:50 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require("discord.js")
-const { PassThrough, Writable } = require("stream")
+const path = require("path")
 const { Chalk } = require("chalk")
 const { GFSManager } = require("./gfs/gfs.js")
 require("dotenv").config()
@@ -14,12 +14,26 @@ class BotWrapper {
       .then(_ => {console.debug("Logged in! Now I am " + this.client.user.tag + " or "+ this.client.user.id)})
       .catch(console.error)
     this.client.on("debug", _ => {console.debug(_)})
-    this.client.on("ready", () => {
-      
+    this.client.on("clientReady", () => {
+      function rdSRecursion(currentDir) {
+        let dir = fs.readdirSync(currentDir, { withFileTypes: true })
+        
+        dir = dir.filter(item => {
+          return item.isDirectory() || item.name.endsWith(".js")
+        })
+        
+        for (const item of dir) {
+          if (item.isDirectory()) {
+            rdSRecursion(path.join(currentDir, item.name))
+          } else {
+            const file = require(path.join(currentDir, item.name))
+            client.on(file.event, file.handler)
+            console.log("Successfully registered module " + file.name)
+          }
+        }
+      }
+      rdSRecursion(path.join(__dirname, "modules"))
     })
-  }
-  initGFSClient(id, sid) {
-    return this.geofs.tryLogin({id, sid})
   }
 }
 
